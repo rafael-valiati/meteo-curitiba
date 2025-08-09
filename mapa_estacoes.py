80464957762360ec03d5247876e2bcf5b4dd2e13
rafael-valiati <44684423+rafael-valiati@users.noreply.github.com>
2025-08-09 11:18:08 -0300

    Update mapa_estacoes.py

--- mapa_estacoes.py
+++ mapa_estacoes.py
@@ -55,7 +55,7 @@ hora_num = datetime.now(brasilia_tz).hour
 
 # Pega dados das estações suspeitas só se for à noite.
 if 5 <= hora_num <= 19:
-    for station in stations[:20]:
+    for station in stations[:21]:
         temp, lat, lon = get_station_temperature(station)
         estacoes.append(station)
         temperatures.append(temp if temp is not None else np.nan)  # Aceitar np.nan
@@ -106,7 +106,7 @@ custom_colormap = ListedColormap(col)
 ctx.providers.keys()  # This will trigger the initialization
 
 # Criação do gráfico usando matplotlib diretamente
-fig, ax = plt.subplots(1, 1, figsize=(10, 7))
+fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 gdf = gpd.GeoDataFrame(dados, geometry=gpd.points_from_xy(dados.Longitude, dados.Latitude), crs="EPSG:4326")
 gdf = gdf.to_crs(epsg=3857)  # Convertendo para o CRS usado pelo contextily
 
@@ -173,20 +173,22 @@ for idx, row in gdf.iterrows():
             ax.text(row.geometry.x, row.geometry.y + 1500, f"Canguiri", color='black', va='center', ha='center', fontsize=8, weight='bold')
         elif idx in [19]:
             ax.text(row.geometry.x, row.geometry.y + 1500, f"Q. Barras", color='black', va='center', ha='center', fontsize=8, weight='bold')
-        if(len(gdf) > 20):
-            if idx in [20]:
+        elif idx in [20]:
+            ax.text(row.geometry.x, row.geometry.y - 1500, f"Borda do Campo", color='black', va='center', ha='center', fontsize=8, weight='bold')
+        if(len(gdf) > 21):
+            if idx in [21]:
                 ax.text(row.geometry.x, row.geometry.y + 1500, f"Vista Alegre", color='black', va='center', ha='center', fontsize=8, weight='bold')
-            elif idx in [21]:
-                ax.text(row.geometry.x, row.geometry.y + 1500, f"S Inácio", color='black', va='center', ha='center', fontsize=8, weight='bold')
             elif idx in [22]:
-                ax.text(row.geometry.x + 500, row.geometry.y - 1500, f"Bigorrilho", color='black', va='center', ha='center', fontsize=8, weight='bold')
+                ax.text(row.geometry.x, row.geometry.y + 1500, f"S Inácio", color='black', va='center', ha='center', fontsize=8, weight='bold')
             elif idx in [23]:
-                ax.text(row.geometry.x - 1000, row.geometry.y + 1500, f"Pilarzinho", color='black', va='center', ha='center', fontsize=8, weight='bold')
+                ax.text(row.geometry.x + 500, row.geometry.y - 1500, f"Bigorrilho", color='black', va='center', ha='center', fontsize=8, weight='bold')
             elif idx in [24]:
-                ax.text(row.geometry.x, row.geometry.y + 1500, f"S Lourenço", color='black', va='center', ha='center', fontsize=8, weight='bold')
+                ax.text(row.geometry.x - 1000, row.geometry.y + 1500, f"Pilarzinho", color='black', va='center', ha='center', fontsize=8, weight='bold')
             elif idx in [25]:
-                ax.text(row.geometry.x, row.geometry.y - 1500, f"Água Verde", color='black', va='center', ha='center', fontsize=8, weight='bold')
+                ax.text(row.geometry.x, row.geometry.y + 1500, f"S Lourenço", color='black', va='center', ha='center', fontsize=8, weight='bold')
             elif idx in [26]:
+                ax.text(row.geometry.x, row.geometry.y - 1500, f"Água Verde", color='black', va='center', ha='center', fontsize=8, weight='bold')
+            elif idx in [27]:
                 ax.text(row.geometry.x, row.geometry.y + 1500, f"Uberaba", color='black', va='center', ha='center', fontsize=8, weight='bold')
         if (33 <= row['Temperatura'] < 40) or (-5 < row['Temperatura'] <= 5):
             ax.text(row.geometry.x, row.geometry.y, f'{row["Temperatura"]:.1f}', color='white', ha='center', va='center', fontsize=10, weight='bold')
